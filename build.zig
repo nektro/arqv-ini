e229c738bbf5cf0d5464bf9178c17813069a7028
Meghan Denny <hello@nektro.net>
2026-06-01 14:47:01 -0700

    update to zig 0.15.2

--- build.zig
+++ build.zig
@@ -2,23 +2,28 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-    const mode = b.option(std.builtin.Mode, "mode", "") orelse .Debug;
+    const mode = b.option(std.builtin.OptimizeMode, "mode", "") orelse .Debug;
     const disable_llvm = b.option(bool, "disable_llvm", "use the non-llvm zig codegen") orelse false;
 
-    const lib = b.addStaticLibrary(.{
+    const lib = b.addLibrary(.{
+        .linkage = .static,
         .name = "ini",
-        .root_source_file = b.path("src/ini.zig"),
-        .target = target,
-        .optimize = mode,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/ini.zig"),
+            .target = target,
+            .optimize = mode,
+        }),
     });
     lib.use_llvm = !disable_llvm;
     lib.use_lld = !disable_llvm;
     b.installArtifact(lib);
 
     const main_tests = b.addTest(.{
-        .root_source_file = b.path("src/ini.zig"),
-        .target = target,
-        .optimize = mode,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/ini.zig"),
+            .target = target,
+            .optimize = mode,
+        }),
     });
     main_tests.use_llvm = !disable_llvm;
     main_tests.use_lld = !disable_llvm;
